9e754bacb257079f4a205a30ee2cef4aabc820e0
shin <sheashinmie04@gmail.com>
2026-05-15 19:26:21 +0800

    Resolved build artifact conflicts

--- obj/Debug/net10.0/ShelterLink.AssemblyInfo.cs
+++ obj/Debug/net10.0/ShelterLink.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShelterLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61db3f02e0ec5de114925783cecd5ec399129c51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db3a361a0c3dc9e2e2d16a421a9238c6b4094ec2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShelterLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShelterLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
